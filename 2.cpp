f9e06382fe9ddf3baaf39d1e9b7799a4f09b669f
upper-class <tearssusliks@outlook.com>
2020-05-04 11:47:05 +0800

    2 Cproject

---
+++ 2.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 2.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 04 May 2020 11:45:09 AM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "hello world" << endl;
+    return 0;
+}
