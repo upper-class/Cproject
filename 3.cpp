6d7dde5ae8905d4699a1c70ab75751415a30a322
upper-class <tearssusliks@outlook.com>
2020-05-04 11:53:08 +0800

    3 Cproject

---
+++ 3.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 3.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 04 May 2020 11:48:49 AM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "hello" << endl;
+    return 0;
+}
